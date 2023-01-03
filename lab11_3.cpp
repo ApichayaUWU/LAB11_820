8a930037a6bdd4e5fc05d2a423420dc4d1266839
apichaya <apichaya_ink@cmu.ac.th>
2023-01-03 16:06:48 +0700

    lab11_3 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    ifstream source;
+    source.open("score.txt");
+
+    string text_score;
+    double i = 0;
+    double sum = 0 , sum2 = 0;
+    double score , mean , sd;
+    
+    while(getline(source,text_score))
+    {
+        score = stod(text_score);
+        sum = sum + score;
+        sum2 = sum2 + pow(score,2);
+        i++;
+    }
+
+    mean = sum / i;
+
+    sd = sqrt((sum2 * (1/i) ) - (pow(mean,2)));
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd;
+}
